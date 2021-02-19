b8a6ba115396898b24ca922061768fe50b9837db
pkkwilliam <pkkwilliam919@gmail.com>
2021-02-19 13:40:21 +0800

    add service executor

---
+++ component/common/applicationComponent.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import ServiceExecutor from "../../service/serviceExecutor";
+import ApplicationContext from "./applicationContext";
+
+export default class ApplicationComponent extends Component {
+  _applicationContext = new ApplicationContext();
+  // _storage = new Storage();
+  _serviceExecutor;
+
+  get applicationContext() {
+    return this._applicationContext;
+  }
+
+  get serviceExecutor() {
+    if (!this._serviceExecutor) {
+      this._serviceExecutor = new ServiceExecutor(
+        this.applicationContext.serviceUrl,
+        () => console.log("saveUserToken"),
+        () => console.log("removeUserToken"),
+        () => console.log("getUserToken"),
+        this.onError
+      );
+    }
+    return this._serviceExecutor;
+  }
+}
